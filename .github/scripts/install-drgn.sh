89f679aca96b80265c385aaa90f61939930ac98e
Serapheim Dimitropoulos <serapheim@delphix.com>
2020-02-19 09:21:05 -0800

    Fix automated testing enviroment dependencies
    Github environment is missing libomp5

--- .github/scripts/install-drgn.sh
+++ .github/scripts/install-drgn.sh
@@ -4,7 +4,7 @@
 # These are build requirements of "drgn"; if we don't install these, the
 # build/install of "drgn" will fail below.
 #
-sudo apt-get install bison flex libelf-dev libdw-dev libomp-dev
+sudo apt-get install bison flex libelf-dev libdw-dev libomp5 libomp-dev
 
 git clone https://github.com/osandov/drgn.git
 
