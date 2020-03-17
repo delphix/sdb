d496a034065ebf247cc96c8790ee33afed30ef1c
Serapheim Dimitropoulos <serapheim@delphix.com>
2020-03-17 21:07:32 +0000

    github-actions: enable testing with multiple python versions github-actions: upgrade to latest stable checkout module (v2)

--- .github/scripts/install-libkdumpfile.sh
+++ .github/scripts/install-libkdumpfile.sh
@@ -2,9 +2,12 @@
 
 #
 # These are build requirements of "libkdumpfile"; if we don't install these,
-# the build/install of "libkdumpfile" will fail below.
+# the build/install of "libkdumpfile" will fail below. Note that we install
+# all version of python3.X-dev so the Github actions jobs can install
+# libkdumpfile with the right version.
 #
-sudo apt-get install autoconf automake liblzo2-dev libsnappy1v5 libtool pkg-config python3-dev zlib1g-dev
+sudo apt-get install autoconf automake liblzo2-dev libsnappy1v5 libtool pkg-config zlib1g-dev
+sudo apt-get install python3.6-dev python3.7-dev python3.8-dev
 
 git clone https://github.com/ptesarik/libkdumpfile.git
 
@@ -14,3 +17,8 @@ autoreconf -fi
 make
 sudo make install
 cd -
+
+#
+# Debug statements
+#
+echo $(which python3)
