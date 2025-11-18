cf987b86181cdfb48f9ef90ff8b5ddc4de6a0406
Dennis Jones <dennis.jones@perforce.com>
2025-11-17 18:11:20 -0800

    DLPX-95965 _framestr() parsing is fragile - should use drgn StackFrame API properties
    PR URL: https://www.github.com/delphix/sdb/pull/356

--- sdb/_version.py
+++ sdb/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.3.1.dev8'
-__version_tuple__ = version_tuple = (0, 3, 1, 'dev8')
+__version__ = version = '0.3.1.dev10'
+__version_tuple__ = version_tuple = (0, 3, 1, 'dev10')
 
-__commit_id__ = commit_id = 'g1375500cf'
+__commit_id__ = commit_id = 'g92f7cd09e'
