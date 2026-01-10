0258770aa0f6944522a081bf17213184eb2f0eca
Serapheim Dimitropoulos <sdimitropoulos@coreweave.com>
2026-01-10 13:49:14 +0000

    Update regression output for commit 3950641 (local variables support with improved stack_trace API)

--- sdb/_version.py
+++ sdb/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.3.1.dev4'
-__version_tuple__ = version_tuple = (0, 3, 1, 'dev4')
+__version__ = version = '0.3.1.dev8'
+__version_tuple__ = version_tuple = (0, 3, 1, 'dev8')
 
-__commit_id__ = commit_id = 'ge7427f777'
+__commit_id__ = commit_id = 'g1375500cf'
